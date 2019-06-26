18a82abc99016e7afb58e8cc3a52444d9c863088
DreamsOfElectricSheep <theofficial.vibe.studios@gmail.com>
2019-06-25 20:37:34 -0700

    2

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 
 int main() {
 
+  auto words = readFile("C:/Users/Tyler Mayxonesing/Desktop/test_files/shake_it_off.txt");
+  int i = 0;
+  while (i < words.size()){
+    std::cout << words.at(i) << std::endl;
+    i++;
+  }
   return 0;
 }
